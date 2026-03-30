2d756b5e6d4ac324c670189297c06c3d7f24d075
muddyrain <qiu3291002845@gmail.com>
2026-03-30 22:20:49 +0800

    feat: 添加激励词汇

--- apps/web/src/layouts/Layout.tsx
+++ apps/web/src/layouts/Layout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import { MotivationalFooter } from '@/components/MotivationalFooter';
+
 import Header from './Header';
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 pb-20">
         <Outlet />
       </main>
+      <MotivationalFooter />
     </div>
   );
 }
